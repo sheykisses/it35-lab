f56b39fd7bd7a2be098eecbac8f03808bb3f3026
sheykisses <dancilhershey65@gmail.com>
2025-04-17 20:48:58 +0800

    added updating App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+
+
+/* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
-
+/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
 
-
+/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
@@ -16,12 +19,22 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* import '@ionic/react/css/palettes/dark.always.css'; */
+/* import '@ionic/react/css/palettes/dark.class.css'; */
 import '@ionic/react/css/palettes/dark.system.css';
 
-
+/* Theme variables */
 import './theme/variables.css';
 import Login from './pages/Login';
 import Menu from './pages/Menu';
+import Register from './pages/Register';
 
 setupIonicReact();
 
@@ -30,10 +43,11 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonRouterOutlet>
         <Route exact path="/it35-lab" component={Login} />
+        <Route exact path="/it35-lab/register" component={Register} />
         <Route path="/it35-lab/app" component={Menu} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
 );
 
-export default App;
+export default App; 
