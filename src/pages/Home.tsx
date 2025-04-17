75ca88317e533cd734e40570f7af4d1bf44c5384
sheykisses <dancilhershey65@gmail.com>
2025-04-17 21:01:57 +0800

    Added new Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,9 +25,9 @@ import {
   const Home: React.FC = () => {
 
     const tabs =[
-      {name:'Feed', tab:'feed',url: '/it35-lab/app/Home/feed', icon: bookOutline},
-      {name:'Search', tab:'search', url: '/it35-lab/app/Home/search', icon: search},
-      {name:'Favorites',tab:'favorites', url: '/it35-lab/app/Home/favorites', icon: star},
+      {name:'Feed', tab:'feed',url: '/it35-lab/app/home/feed', icon: bookOutline},
+      {name:'Search', tab:'search', url: '/it35-lab/app/home/search', icon: search},
+      {name:'Favorites',tab:'favorites', url: '/it35-lab/app/home/favorites', icon: star},
       ]
 
     
