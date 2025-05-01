64b8edb28fd14b2a0652f30ef800cf6db62debcd
sheykisses <dancilhershey65@gmail.com>
2025-05-01 17:29:15 +0800

    added a new home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,40 @@
 import { 
   IonButton,
-  IonButtons,
-  IonContent, 
-  IonHeader, 
-  IonIcon, 
-  IonLabel, 
-  IonMenuButton, 
-  IonPage, 
-  IonRouterOutlet, 
-  IonTabBar, 
-  IonTabButton, 
-  IonTabs, 
-  IonTitle, 
-  IonToolbar 
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonIcon, 
+      IonLabel, 
+      IonMenuButton, 
+      IonPage, 
+      IonRouterOutlet, 
+      IonTabBar, 
+      IonTabButton, 
+      IonTabs, 
+      IonTitle, 
+      IonToolbar 
   } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { bookOutline, search, star } from 'ionicons/icons';
+import { Route, Redirect } from 'react-router';
 
-  import {IonReactRouter} from '@ionic/react-router';
-  import { bookOutline, search, star } from 'ionicons/icons';
-  import { Route, Redirect } from 'react-router';
-  import Favorites from './home-tabs/Favorites';
-  import Feed from './home-tabs/Feed';
-  import Search from './home-tabs/Search';
-
+import Favorites from './home-tabs/Favorites';
+import Feed from './home-tabs/Feed';
+import Search from './home-tabs/Search';
+  
   const Home: React.FC = () => {
 
-    const tabs =[
+    const tabs = [
       {name:'Feed', tab:'feed',url: '/it35-lab/app/home/feed', icon: bookOutline},
       {name:'Search', tab:'search', url: '/it35-lab/app/home/search', icon: search},
       {name:'Favorites',tab:'favorites', url: '/it35-lab/app/home/favorites', icon: star},
-      ]
- 
-return (
-  <IonReactRouter>
+    ]
+    
+    return (
+      <IonReactRouter>
         <IonTabs>
           <IonTabBar slot="bottom">
+
             {tabs.map((item, index) => (
               <IonTabButton key={index} tab={item.tab} href={item.url}>
                 <IonIcon icon={item.icon} />
@@ -43,16 +44,19 @@ return (
             
           </IonTabBar>
         <IonRouterOutlet>
+
           <Route exact path="/it35-lab/app/home/feed" render={Feed} />
           <Route exact path="/it35-lab/app/home/search" render={Search} />
           <Route exact path="/it35-lab/app/home/favorites" render={Favorites} />
+
           <Route exact path="/it35-lab/app/home">
             <Redirect to="/it35-lab/app/home/feed" />
           </Route>
+
         </IonRouterOutlet>
         </IonTabs>
       </IonReactRouter>
-  );
-};
-
-export default Home;
+    );
+  };
+  
+  export default Home;
--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -7,6 +7,7 @@ import {
     IonTitle, 
     IonToolbar 
 } from '@ionic/react';
+import FeedContainer from '../../components/FeedContainer';
 
 const Feed: React.FC = () => {
   return (
@@ -20,6 +21,17 @@ const Feed: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+          Feed
+        </div>
+        <FeedContainer />
       </IonContent>
     </IonPage>
   );
